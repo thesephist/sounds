d82bb278f7a3ce14101d275a2e83920aee319a43
Linus Lee <linus@thesephist.com>
2019-01-19 21:16:50 -0800

    Add litterate to generate literate documentation from `yarn docs`

---
+++ litterate.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: 'Sounds collection',
+    description: 'A collection of sounds from places I\'ve been',
+    files: [
+        './src/**/*.js',
+        './static/**/*.js',
+    ],
+}
+
